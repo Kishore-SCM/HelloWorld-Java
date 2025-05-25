e3fb54280241e74873e91d1b84f5ca137809ac98
root <root@DESKTOP-I2NIK36.>
2025-05-25 07:14:31 +0000

    Hello world java file

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("this is madhu");
+    System.out.println("Hello, Banglore");
+    System.out.println("Hello, canada");
+    System.out.println("Hello, Devops World ");
+    System.out.println("Hello, This is Kishore ");
+    System.out.println("Hello, wed......................");
+  }
+}
